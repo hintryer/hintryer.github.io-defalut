863658322ef74d91da935e055ef9cfe61f236d5b
hintryer <gindeny+github@gmail.com>
2024-08-11 13:21:08 +0800

    Create fetch_data.py

---
+++ ceshi/fetch_data.py
@@ -0,0 +1,12 @@
+import requests
+
+def fetch_data(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+url = 'https://www.gitlink.org.cn/api/yi-c/yd/raw/sy.json?ref=master'
+data = fetch_data(url)
+print(data)
